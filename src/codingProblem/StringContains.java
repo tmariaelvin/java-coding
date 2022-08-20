4e3536977d203c8eaf9f5b27fdc2c67d30c9bc57
Elvin-Provility <elvin@provility.com>
2022-08-20 17:09:17 +0530

    added gitignore

--- src/codingProblem/StringContains.java
+++ src/codingProblem/StringContains.java
@@ -3,7 +3,8 @@ package codingProblem;
 public class StringContains {
     public static void main(String[] args){
         String name =  "elvin antony";
-        String subName = "vin";
+        String subName = "win";
+        System.out.println( name.contains(subName));
 
     }
 }
