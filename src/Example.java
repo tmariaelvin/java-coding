d57d8d6b94864a281ec98bd900034209cc5b3eba
tmariaelvin <tmariaelvin@gmail.com>
2023-07-11 22:19:54 +0530

    New commit message

---
+++ src/Example.java
@@ -0,0 +1,36 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Example {
+    private static final String EMAIL_REGEX_PATTERN = "^[\\w-\\. +\"]+@([\\w-\\[]+\\.)+[\\w-\\]]{2,7}$";
+    public static boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_REGEX_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public static void main(String[] args) {
+        String[] emails = {
+                "email@example.com",
+                "firstname.lastname@example.com",
+                "email@subdomain.example.com",
+                "firstname+lastname@example.com",
+                "email@123.123.123.123",
+                "email@[123.123.123.123]",
+                "\"email\"@example.com",
+                "1234567890@example.com",
+                "email@example-one.com",
+                "_______@example.com",
+                "email@example.name",
+                "email@example.museum",
+                "mail@example.co.jp",
+                "firstname-lastname@example.com",
+                "email@123.123.123.1",
+        };
+
+        for (String email : emails) {
+            boolean isValid = isValidEmail(email);
+            System.out.println(email + ": " + isValid);
+        }
+    }
+}
---
+++ src/ohoz/Z1.java
@@ -0,0 +1,7 @@
+package ohoz;
+
+public class Z1 {
+    public static void main(String[] args) {
+        
+    }
+}
